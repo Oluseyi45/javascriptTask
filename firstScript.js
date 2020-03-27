9b795182e3becd5ab56efc245a8200a7fc095550
Oluseyi45 <62138621+Oluseyi45@users.noreply.github.com>
2020-03-27 10:01:25 +0100

    Create firstScript.js

---
+++ firstScript.js
@@ -0,0 +1,13 @@
+// assign my  namme to a variable 
+var myName= "Oshoneye Jacob Oluseyi";
+// assign my  courses to an array 
+var myCourses=["HTML", "CSS", "JAVASCRIPT"];
+// create an empty array 
+var array=[];
+//do an iteration using for loop and store results (odd numbers) in the array 
+for (var i =1; i <= 200; i +=2 ){
+  array.push(i);
+  }
+console.log(myName)
+console.log(myCourses)
+console.log(array)
